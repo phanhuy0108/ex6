b4bbe86c16f0537580e26905252a4f7c7ab2f7e2
Phan Huy <phanhuy0108@gmail.com>
2019-10-16 17:57:19 +0700

    done

---
+++ Problem1/Problem1/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    public class Account
+    {
+        int acc = 0;
+        public Account(int amount)
+        {
+            acc = amount;
+        }
+        public void Deposit(int amount)
+        {
+            Console.Write("so tien nap: ");
+            acc = int.Parse(Console.ReadLine());
+            amount += acc;
+        }
+        public void Withdraw(int amount)
+        {
+            Console.Write("so tien rut: ");
+            acc = int.Parse(Console.ReadLine());
+            amount -= acc;
+        }
+        public int Balance()
+        {
+            return acc;
+        }
+    }
+}
---
+++ Problem1/UnitTestAccount/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestAccount")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestAccount")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("39e4f2bf-e1af-4f5e-ac9d-d1e7a99b2aa6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
