f9d1de954b9d0e43a6bc2cbef7dd93861f9ef54f
HuyenNgu <yurikawsaoki@gmail.com>
2019-10-24 17:11:51 +0700

    done 6b

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap Vao Size Mang");
+            int size = int.Parse(Console.ReadLine());
+            Stack s = new Stack(size);
+            s.Push(5);
+            s.Push(3);
+            s.Push(6);
+            s.Push(5);
+            Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Peek());
+            Console.WriteLine(s.Count());
+            s.Clear();
+
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Stack
+    {
+        private int[] Buffer;
+        private int Top;
+        public Stack(int size)
+        {
+            Buffer = new int[size];
+            Top = -1;
+        }
+        public void Push (int v)
+        {
+            Buffer[Top + 1]=v;
+            Top++;
+        }
+        public int Pop()
+        {
+            int n = Buffer[Top];
+            Top--;
+            return n;
+        }
+        public int Peek()
+        {
+          return Buffer[Top];
+        }
+        public int Count()
+        {
+          return Top + 1;
+        }
+        public void Clear()
+        {
+            Top = -1;
+        }
+    }
+}
